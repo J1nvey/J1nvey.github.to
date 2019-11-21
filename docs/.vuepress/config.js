52deab556f1b1e914cbe7524f2b9e9f08674d63b
J1nvey <1308856577@qq.com>
2019-11-21 15:05:03 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    title: 'J1nvey',
+    description: '',
+    base: '/blog/',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ],
+    themeConfig: {
+        search: false,
+        nav: [
+            {
+                text: 'VUE',
+                items: [
+                    { text: 'a1', link: '/pages/test/test1'},
+                    { text: 'a2', link: '/pages/test/test3'}
+                ]
+            }
+        ],
+        sidebar: {
+            '/pages/test/': [
+                {
+                    title: '测试菜单1',
+                    collapsable: false, // false为默认展开菜单, 默认值true是折叠
+                    children: [
+                        ['test1', '子菜单1'],
+                        ['test2', '子菜单2']
+                    ]
+                }
+            ]
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /img/logo.png
+heroText: J1nvey's Study
+tagline: 动手写一写记录一下
+actionText: GET'EM
+actionLink: /pages/test/test1.md
+# features:
+# - title: ♥前端♥
+#   details: 写一写，泡一泡，扭一扭。
+# - title: ♥杂务♥
+#   details: 瞎写。
+# footer: FBI WARINING
+---
